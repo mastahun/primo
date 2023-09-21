f343a94ee8335c0f654e55f143e3b435f14b28e3
educationaltoast <educationaltoast@outlook.com>
2023-09-21 12:41:03 +0600

    downloaded chess icons

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const squares = document.getElementsByClassName('p-4');
 for (let i = 0; i < 64; i++ ){
     squares[i].textContent = String(squares[i].classList[5]) + String(squares[i].classList[4])
 }
+
+
+// factories for chess pieces
+
+const baseChessPiece = (letter, number) => {
+    let isAlive = true;
+    
+}
